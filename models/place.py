f37c40ff1c6bdec30b64602e958bc9c33364e431
azzaomer <azzaomer70@gmail.com>
2024-06-30 05:16:55 +0300

    place and amenity update

--- models/place.py
+++ models/place.py
@@ -1,70 +1,48 @@
 #!/usr/bin/python3
-"""Place Module for HBNB project."""
-import models
-from os import getenv
-from models.base_model import Base, BaseModel
-from models.review import Review
-from models.amenity import Amenity
+""" Place Module for HBNB project """
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from models.amenity import Amenity
+from models.review import Review
 
 
-place_amenity = Table(
-        "place_amenity",
-        Base.metadata,
-        Column("place_id", String(60), ForeignKey("places.id")),
-        Column("amenity_id", String(60), ForeignKey("amenities.id"))
-        )
+# Table for many-to-many relationship between Place and Amenity
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
 
 class Place(BaseModel, Base):
-    """A place to stay."""
+    """ A place to stay """
+    __tablename__ = 'places'
 
-    __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
 
-    if getenv('HBNB_TYPE_STORAGE') == "db":
-        reviews = relationship("Review", backref="place", cascade="delete")
-        amenities = relationship("Amenity", secondary="place_amenity",
-                back_populates="place_amenities", viewonly=False)
-    else:
-        @property
-        def reviews(self):
-            """Review getter."""
-            return [o for o in models.storage.all(Review)
-                    if o.place_id == self.id]
-        @property
-        def amenities(self):
-            """Amenties getter"""
-            return [o for o in models.all(Amenity)
-                    if o.place_id == self.id]
-        @amenities.setter
-        def amenities(self, obj):
-            """Amenities setter"""
-            if isinstance(obj, Amenity):
-                self.amenity_ids.append(obj.id)
+    user = relationship("User", back_populates="places")
+    city = relationship("City", back_populates="places")
+    reviews = relationship("Review", back_populates="place", cascade="all, delete, delete-orphan")
+    amenities = relationship("Amenity", secondary=place_amenity, viewonly=False, back_populates="place_amenities")
 
-    def __init__(self, *args, **kwargs):
-        """Init method."""
-        filtered_kwargs = {k: v for k, v in kwargs.items()
-                           if hasattr(self, k) or k == "id"}
-        super().__init__(*args, **filtered_kwargs)
-        self.name = kwargs.get("name", None)
-        self.description = kwargs.get("description", None)
-        self.number_rooms = kwargs.get("number_rooms", 0)
-        self.number_bathrooms = kwargs.get("number_bathrooms", 0)
-        self.max_guest = kwargs.get("max_guest", 0)
-        self.price_by_night = kwargs.get("price_by_night", 0)
-        self.city_id = kwargs.get("city_id", None)
-        self.user_id = kwargs.get("user_id", None)
-        self.latitude = kwargs.get("latitude", None)
-        self.longitude = kwargs.get("longitude", None)
+    @property
+    def amenities(self):
+        """Getter attribute that returns the list of Amenity instances based on the attribute amenity_ids"""
+        from models import storage
+        from models.amenity import Amenity
+        all_amenities = storage.all(Amenity)
+        place_amenities = [amenity for amenity in all_amenities.values() if amenity.id in self.amenity_ids]
+        return place_amenities
+
+    @amenities.setter
+    def amenities(self, amenity_obj):
+        """Setter attribute that handles append method for adding an Amenity.id to the attribute amenity_ids"""
+        if isinstance(amenity_obj, Amenity):
+            self.amenity_ids.append(amenity_obj.id)
