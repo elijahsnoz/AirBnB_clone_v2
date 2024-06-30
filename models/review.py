4f2790a99943a481a3a25108ea01521f8c7c2f2b
azzaomer <azzaomer70@gmail.com>
2024-06-29 19:42:22 -0700

    fix bugs

--- models/review.py
+++ models/review.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
+
 class Review(BaseModel, Base):
     """ Review class to store review information """
     __tablename__ = 'reviews'
@@ -11,6 +12,5 @@ class Review(BaseModel, Base):
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-
     place = relationship("Place", back_populates="reviews")
     user = relationship("User", back_populates="reviews")
